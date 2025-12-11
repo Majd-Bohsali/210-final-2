cd9e5bc8940422985bc131df067f24f84bf940c5
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-10 18:35:09 -0800

    Milestone 2

--- 210-final-2.cpp
+++ 210-final-2.cpp
@@ -10,17 +10,27 @@ struct Customer {
     Customer* next; 
 };
 
+const int NEW_CUSTOMER_PROP = 50; 
+
 int main() {
     srand(time(NULL));
-    int namesCount = 10, drinksCount = 10; 
-    string names[] = {"Caleb", "Zoe", "Jackson", "Lila", "Mateo", "Chloe", "Lucas", "Hazel", "Aiden", "Nora"};
-    string drinks[] = {"Vanilla Cold Brew", "Iced Mocha", "Flat White", "Americano", "Chai Latte",
-                        "Matcha Latte", "Hot Chocolate", "Pumpkin Spice Latte",
-                        "Honey Almond Latte", "Iced Espresso"};
+    int namesCount = 20, drinksCount = 20; 
+    string names[] = {
+        "Caleb", "Zoe", "Jackson", "Lila", "Mateo", "Chloe", "Lucas", "Hazel",
+        "Aiden", "Nora", "Emma", "Liam", "Olivia", "Noah", "Ava", "Mason",
+        "Isabella", "Elijah", "Sophia", "Ethan"
+                    };
+    string drinks[] = {
+        "Vanilla Cold Brew", "Iced Mocha", "Flat White", "Americano", "Chai Latte",
+        "Matcha Latte", "Hot Chocolate", "Pumpkin Spice Latte", "Honey Almond Latte",
+        "Iced Espresso", "Caramel Macchiato", "Hazelnut Latte", "Mocha Frappé",
+        "Cinnamon Dolce Latte", "London Fog Tea", "Dirty Chai", "Iced Matcha Lemonade",
+        "White Chocolate Mocha", "Cold Foam Cold Brew", "Peppermint Mocha"
+    };
     
     Customer* head = nullptr; 
 
-    for(int i = 0; i < 5; i++) { 
+    for(int i = 0; i < 3; i++) { 
         Customer* tempCustomer = new Customer; 
         tempCustomer->name = names[rand() % namesCount]; 
         tempCustomer->order = drinks[rand() % drinksCount];
@@ -47,6 +57,26 @@ int main() {
         } else { 
             cout << "Coffee queue is currently empty" << endl;
         }
+
+        if(rand() % 100 < NEW_CUSTOMER_PROP) {
+            Customer* tempCustomer = new Customer; 
+            tempCustomer->name = names[rand() % namesCount]; 
+            tempCustomer->order = drinks[rand() % drinksCount];
+            tempCustomer->next = nullptr;  
+
+            cout << tempCustomer->name << " joined the coffee line" << endl;
+
+            if(head == nullptr) { 
+            head = tempCustomer; 
+            } else { 
+                Customer* currCustomer = head; 
+                while(currCustomer->next) { 
+                    currCustomer = currCustomer->next; 
+                }
+                currCustomer->next = tempCustomer; 
+            }
+        }
+        cout << endl;
     }
 
     return 0;
