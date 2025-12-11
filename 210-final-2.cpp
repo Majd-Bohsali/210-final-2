857faecc3440c621e6e021fad8e660e7302d553b
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-10 18:45:56 -0800

    writting milestone 3

--- 210-final-2.cpp
+++ 210-final-2.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <deque> 
 using namespace std; 
 
 struct Customer { 
@@ -14,7 +15,7 @@ const int NEW_CUSTOMER_PROP = 50;
 
 int main() {
     srand(time(NULL));
-    int namesCount = 20, drinksCount = 20; 
+    int namesCount = 20, drinksCount = 20, muffinCount = 20;; 
     string names[] = {
         "Caleb", "Zoe", "Jackson", "Lila", "Mateo", "Chloe", "Lucas", "Hazel",
         "Aiden", "Nora", "Emma", "Liam", "Olivia", "Noah", "Ava", "Mason",
@@ -27,8 +28,18 @@ int main() {
         "Cinnamon Dolce Latte", "London Fog Tea", "Dirty Chai", "Iced Matcha Lemonade",
         "White Chocolate Mocha", "Cold Foam Cold Brew", "Peppermint Mocha"
     };
-    
-    Customer* head = nullptr; 
+    string muffins[] = {
+    "Blueberry Muffin", "Chocolate Chip Muffin", "Banana Nut Muffin",
+    "Lemon Poppy Seed Muffin", "Cinnamon Streusel Muffin", "Pumpkin Spice Muffin",
+    "Apple Crumble Muffin", "Double Chocolate Muffin", "Raspberry White Chocolate Muffin",
+    "Strawberry Muffin", "Coffee Cake Muffin", "Zucchini Muffin",
+    "Carrot Muffin", "Maple Pecan Muffin", "Peach Cobbler Muffin",
+    "Cranberry Orange Muffin", "Blackberry Muffin", "Almond Muffin",
+    "Mocha Muffin", "Nutella Swirl Muffin"
+    };
+
+    Customer* coffeeHead = nullptr; 
+    deque<Customer> muffinsDqeue; 
 
     for(int i = 0; i < 3; i++) { 
         Customer* tempCustomer = new Customer; 
@@ -36,10 +47,10 @@ int main() {
         tempCustomer->order = drinks[rand() % drinksCount];
         tempCustomer->next = nullptr; 
 
-        if(head == nullptr) { 
-            head = tempCustomer; 
+        if(coffeeHead == nullptr) { 
+            coffeeHead = tempCustomer; 
         } else { 
-            Customer* currCustomer = head; 
+            Customer* currCustomer = coffeeHead; 
             while(currCustomer->next) { 
                 currCustomer = currCustomer->next; 
             }
@@ -49,10 +60,10 @@ int main() {
 
     for(int i = 0; i < 10; i++) { 
         cout << "Round " << i+1 << ":" << endl;
-        if(head) { 
-            Customer* frontCustomer = head;
+        if(coffeeHead) { 
+            Customer* frontCustomer = coffeeHead;
             cout << frontCustomer->name << " was served " << frontCustomer->order << endl; 
-            head = frontCustomer->next; 
+            coffeeHead = frontCustomer->next; 
             delete frontCustomer; 
         } else { 
             cout << "Coffee queue is currently empty" << endl;
@@ -66,15 +77,29 @@ int main() {
 
             cout << tempCustomer->name << " joined the coffee line" << endl;
 
-            if(head == nullptr) { 
-            head = tempCustomer; 
+            if(coffeeHead == nullptr) { 
+            coffeeHead = tempCustomer; 
             } else { 
-                Customer* currCustomer = head; 
+                Customer* currCustomer = coffeeHead; 
                 while(currCustomer->next) { 
                     currCustomer = currCustomer->next; 
                 }
                 currCustomer->next = tempCustomer; 
             }
+        }
+        cout << endl;
+
+        if(!muffinsDqeue.empty()) { 
+            
+        } else { 
+            cout << "Muffins queue is currently empty" << endl;
+        }
+
+        if(rand() % 100 < NEW_CUSTOMER_PROP) {
+            Customer* tempCustomer = new Customer; 
+            tempCustomer->name = names[rand() % namesCount]; 
+            tempCustomer->order = muffins[rand() % muffinCount];
+            muffinsDqeue.push_back(tempCustomer);
         }
         cout << endl;
     }
