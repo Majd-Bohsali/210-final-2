5a5f33728b55f9984f1ef7185cba00daf35ac4c2
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-10 18:07:12 -0800

    writting milestone 1

--- 210-final-2.cpp
+++ 210-final-2.cpp
@@ -2,6 +2,20 @@
 
 using namespace std; 
 
+struct Customer { 
+    string name;
+    string order; 
+    Customer* next; 
+};
+
 int main() {
+
+    string names[] = {"Caleb", "Zoe", "Jackson", "Lila", "Mateo", "Chloe", "Lucas", "Hazel", "Aiden", "Nora"};
+    string drinks[] = {"Vanilla Cold Brew", "Iced Mocha", "Flat White", "Americano", "Chai Latte",
+                        "Matcha Latte", "Hot Chocolate", "Pumpkin Spice Latte",
+                        "Honey Almond Latte", "Iced Espresso"};
+    
+    Customer* coffeListHead = nullptr; 
+
     return 0;
 }
